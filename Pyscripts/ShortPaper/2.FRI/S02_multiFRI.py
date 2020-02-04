09fa3be5ccf22f76fadd8360bd042fbb3ca91072
ArdenB <arden.burrell@gmail.com>
2020-02-04 14:24:10 +1100

    tweaks

--- Pyscripts/ShortPaper/2.FRI/S02_multiFRI.py
+++ Pyscripts/ShortPaper/2.FRI/S02_multiFRI.py
@@ -153,14 +153,14 @@ def FRIcal(ds_ann, mask, dsn, force, ppath, mwbox, data):
 		
 		print("Mask reload:", pd.Timestamp.now())
 		mask_sum = xr.open_dataset(tpath+tMnme)
-		ipdb.set_trace()
 		# This is so i can count the number of values that are valid in each location
 
 		# ===== Calculate the Moving window =====
 		dsan_lons = ds_ann.rolling({"longitude":SF}, center = True, min_periods=1).mean() 
 		ipdb.set_trace()
 
-		dsan_lons = tempNCmaker(dsan_lons, tpath, tname, "AnBF", {'latitude': 500}, readchunks={'longitude': 500}, skip=True)
+		dsan_lons = tempNCmaker(dsan_lons, tpath, tname, "AnBF", {'latitude': 500}, readchunks={'longitude': 500}, skip=False)
+		ipdb.set_trace()
 		
 		# dsan_lons = dsan_lons.where(mask["mask"].values == 1)
 		# ds_con = ds_con.where(mask.mask.values == 1)
