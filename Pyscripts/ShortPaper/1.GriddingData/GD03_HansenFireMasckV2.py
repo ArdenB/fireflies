f8fcffe7fcf5ba7429e5c7d803e0e42799716d91
arden <arden.burrell@gmail.com>
2020-02-12 16:03:13 +1100

    tweaks

--- Pyscripts/ShortPaper/1.GriddingData/GD03_HansenFireMasckV2.py
+++ Pyscripts/ShortPaper/1.GriddingData/GD03_HansenFireMasckV2.py
@@ -87,14 +87,102 @@ def main():
 
 	# ========== Make the files ==========
 	fnames = ActiveFireMask(dpath, force)
+	
+	# ========== Build some netcdf versions ==========
+	fns_NC = MODIS_shptoNC(dpath, fnames, force, ymin=2001, ymax=2002, dsn = "esacci")
 
-
-	# ========== Loop over the datasets ==========
-	HansenMasker(fnames, force, dpath, ymin=2001, ymax=2019, )
+	# ========== Resample the Hansen ==========
+	Hansen_resizer(dpath, force, fns_NC, ymin=2001, ymax=2019, dsn = "esacci")
 	ipdb.set_trace()
+	# ========== Loop over the datasets ==========
+	# HansenMasker(fnames, force, dpath, ymin=2001, ymax=2019, )
 
 
 #==============================================================================
+
+def Hansen_resizer(dpath, force, fns_NC, ymin=2001, ymax=2019, dsn = "esacci"):
+	"""
+	take the Hansen data and resize it to match the datagrid
+	"""
+
+	# ========== Loop over the datasets ==========	
+	for yr in range(ymin, ymax):
+		# ========== load in the hansen file ==========
+		fname = dpath + "/BurntArea/HANSEN/lossyear/Hansen_GFC-2018-v1.6_lossyear_SIBERIA.nc"
+		ds_in = xr.open_dataset(fname, chunks={'latitude': 10000, 'longitude':1000})
+		ds_in = ds_in.sel(dict(latitude=slice(70.0, 30.0), longitude=slice(-10.0, 180.0)))
+
+		# ========== Find the places that were lost in each year ===========
+		ds_BOOL = ds_in == yr-2000
+
+		# ========== Open the resolution dataset ===========
+		ds_res = xr.open_dataset(fns_NC[0])
+
+		SF_lat = int(np.round(abs(np.unique(np.round(np.diff(ds_res.latitude.values), decimals=6))[0])/abs(np.unique(np.round(np.diff(ds_in.latitude.values), decimals=6))[0])))
+		SF_lon = int(np.round(abs(np.unique(np.round(np.diff(ds_res.longitude.values), decimals=6))[0])/abs(np.unique(np.round(np.diff(ds_in.longitude.values), decimals=6))[0])))
+
+
+		ds_course = ds_BOOL.coarsen(dim={"latitude":SF_lat, "longitude":SF_lon}, boundary="pad").mean()
+		ipdb.set_trace()
+
+	ipdb.set_trace()
+	
+
+# def function():
+# 	pass
+
+def MODIS_shptoNC(dpath, fnames, force, ymin=2001, ymax=2019, dsn = "esacci"):
+	""" 
+	Function to open the shape file and get the values into a raster at a workable 
+	resolu
+	"""
+	# ========== Open the Raster dataset ==========
+	fname = dpath + "/masks/landwater/%s_landwater.nc" % dsn
+	ds_in = xr.open_dataset(fname, chunks={'latitude': 10000, 'longitude':1000})
+	ds_in = ds_in.sortby("latitude", ascending=False)
+	ds_in = ds_in.sel(dict(latitude=slice(70.0, 30.0), longitude=slice(-10.0, 180.0)))
+
+	fnames_nc = []
+	
+	# ========== Open the vector dataset ==========	
+	for yr in range(ymin, ymax):
+
+		print("Starting the boolean netcdf creation for %d at:" %  yr, pd.Timestamp.now())
+		# ========== Setup the year ==========
+		date  = datefixer(yr, 12, 31)
+
+		fnout = dpath + "/BurntArea/MODIS_ActiveFire/AnnualActiveFire_%d_%s_res.nc" % (yr, dsn)
+
+		if force or not os.path.isfile(fnout):
+
+			# creat the file name of the results
+			fn = dpath + "/BurntArea/MODIS_ActiveFire/AnnualActiveFire%d.shp" % yr
+
+			# load the shape
+			afr    = gpd.read_file(fn)
+			shapes = [(shape, n+1) for n, shape in enumerate(afr.geometry)]
+
+			transform = transform_from_latlon(ds_in['latitude'], ds_in['longitude'])
+			out_shape = (len(ds_in['latitude']), len(ds_in['longitude']))
+			raster    = features.rasterize(shapes, out_shape=out_shape,
+			                            fill=0, transform=transform, dtype="int16")
+			raster = raster.astype(bool)
+
+			ds_out = ds_in.copy(data={"landwater":raster}).rename({"landwater":"ActiveFire"})
+			ds_out.attrs["title"]       = "ActiveFireData"
+			ds_out.attrs["summary"]     = "Activefire" 
+			ds_out.attrs["Conventions"] = "CF-1.7"
+			ds_out["time"]              = date["time"]
+			
+			# ++++++++++ Data Provinance ++++++++++ 
+			ds_out.attrs["history"]             = "%s: Netcdf file created using %s (%s):%s by %s" % (
+				str(pd.Timestamp.now()), __title__, __file__, __version__, __author__) + ds_out.attrs["history"]
+
+			# ++++++++++ Write the data out ++++++++++ 
+			ds_out = tempNCmaker(ds_out, fnout, "ActiveFire", chunks={'latitude': 10000, 'longitude':1000}, skip=False, name="%d Active FIre " % yr)
+
+		fnames_nc.append(fnout)
+	return fnames_nc
 
 def ActiveFireMask(dpath, force, ymin=2001, ymax=2019):
 	def _annualfire(actfire, yr):
@@ -119,13 +207,16 @@ def ActiveFireMask(dpath, force, ymin=2001, ymax=2019):
 		actfire.to_file(outfn)
 	
 	# ========== Build the results ==========
-	path  = dpath + "BurntArea/MODIS_ActiveFire/DL_FIRE_M6_85602/fire_archive_M6_85602.shp"
+	path  = dpath + "/BurntArea/MODIS_ActiveFire/DL_FIRE_M6_85602/fire_archive_M6_85602.shp"
 	
 
 
-	fnames = [dpath + "BurntArea/MODIS_ActiveFire/AnnualActiveFire%d.shp" % yr for yr in range(ymin, ymax)]
+	fnames = [dpath + "/BurntArea/MODIS_ActiveFire/AnnualActiveFire%d.shp" % yr for yr in range(ymin, ymax)]
 
 	if not all([os.path.isfile(fn) for fn in fnames]) or force:
+		ipdb.set_trace()
+
+
 		# ========== Load the data ==========
 		print("starting active fire data read at:	", pd.Timestamp.now())
 		afr = gpd.read_file(path)
@@ -157,7 +248,64 @@ def ActiveFireMask(dpath, force, ymin=2001, ymax=2019):
 
 	# ========== Load the data ==========
 	return fnames
+
 #==============================================================================
+def tempNCmaker(ds, fnout, vname, chunks={'latitude': 10000, 'longitude':1000}, skip=False, name="tmp"):
+
+	""" Function to Quickly save netcdfs"""
+	
+	if type(vname) == list:
+		encoding = OrderedDict()
+		for vn in vname:
+			encoding[vn] = {'shuffle':True,'zlib':True,'complevel':5}
+	else:
+		encoding =  ({vname:{'shuffle':True,'zlib':True,'complevel':5}})
+	if not all([skip, os.path.isfile(fnout)]):
+		delayed_obj = ds.to_netcdf(fnout, 
+			format         = 'NETCDF4', 
+			encoding       = encoding,
+			unlimited_dims = ["time"],
+			compute=False)
+
+		print("Starting write of %s data at" % name, pd.Timestamp.now())
+		with ProgressBar():
+			results = delayed_obj.compute()
+	dsout = xr.open_dataset(fnout, chunks=chunks) 
+	return dsout
+
+
+def transform_from_latlon(lat, lon):
+    lat = np.asarray(lat)
+    lon = np.asarray(lon)
+    trans = Affine.translation(lon[0], lat[0])
+    scale = Affine.scale(lon[1] - lon[0], lat[1] - lat[0])
+    return trans * scale
+
+
+def datasets(dpath, chunksize):
+	# ========== set the filnames ==========
+	data= OrderedDict()
+	data["COPERN_BA"] = ({
+		'fname':dpath+"/masks/landwater/",
+		'var':"BA", "gridres":"300m", "region":"Global", "timestep":"AnnualMax",
+		"start":2014, "end":2019,"rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize}, 
+		"rename":{"lon":"longitude", "lat":"latitude"}
+		})
+
+	data["MODIS"] = ({
+		"fname":dpath+"/BurntArea/MODIS/MODIS_MCD64A1.006_500m_aid0001_reprocessedBAv2.nc",
+		'var':"BA", "gridres":"500m", "region":"Siberia", "timestep":"Annual", 
+		"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1,'longitude': chunksize, 'latitude': chunksize},
+		"rename":None, "maskfn":"/media/ubuntu/Seagate Backup Plus Drive/Data51/BurntArea/MODIS/MASK/MCD12Q1.006_500m_aid0001v2.nc"
+		})
+	data["esacci"] = ({
+		"fname":dpath+"/BurntArea/esacci/processed/esacci_FireCCI_*_burntarea.nc",
+		'var':"BA", "gridres":"250m", "region":"Asia", "timestep":"Annual", 
+		"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize},
+		"rename":None, "maskfn":"/media/ubuntu/Seagate Backup Plus Drive/Data51/BurntArea/esacci/processed/esacci_landseamask.nc"
+		# "rename":{"band":"time","x":"longitude", "y":"latitude"}
+		})
+
 
 def syspath():
 	# ========== Create the system specific paths ==========
@@ -182,6 +330,33 @@ def syspath():
 	else:
 		ipdb.set_trace()
 	return dpath, chunksize	
+
+def datefixer(year, month, day):
+	"""
+	Opens a netcdf file and fixes the data, then save a new file and returns
+	the save file name
+	args:
+		ds: xarray dataset
+			dataset of the xarray values
+	return
+		time: array
+			array of new datetime objects
+	"""
+
+
+	# ========== create the new dates ==========
+	# +++++ set up the list of dates +++++
+	dates = OrderedDict()
+	tm = [dt.datetime(int(year) , int(month), int(day))]
+	dates["time"] = pd.to_datetime(tm)
+
+	dates["calendar"] = 'standard'
+	dates["units"]    = 'days since 1900-01-01 00:00'
+	
+	dates["CFTime"]   = date2num(
+		tm, calendar=dates["calendar"], units=dates["units"])
+
+	return dates
 
 #==============================================================================
 
