15220ae1f9ddbfe004b0f1a88aaf8819af9535a1
Arden Burrell <arden.burrell@gmail.com>
2020-03-12 10:26:59 -0400

    tweaks

--- Pyscripts/ShortPaper/2.FRI/S02_multiFRI.py
+++ Pyscripts/ShortPaper/2.FRI/S02_multiFRI.py
@@ -99,6 +99,7 @@ def main():
 		# ========== work out the FRI ==========
 		FRIcal(ds_ann, mask, dsn, force, ppath, mwbox, data, chunksize)
 		# force = False
+		print(dsn, " Complete at:", pd.Timestamp.Now())
 		
 	ipdb.set_trace()
 
@@ -160,7 +161,7 @@ def FRIcal(ds_ann, mask, dsn, force, ppath, mwbox, data, chunksize):
 		
 		print("Mask reload:", pd.Timestamp.now())
 		mask_sum = xr.open_dataset(tpath+tMnme)
-		continue
+		# continue
 		# This is so i can count the number of values that are valid in each location
 
 		# ===== Calculate the Moving window on dim 1 =====
@@ -371,12 +372,12 @@ def datasets(dpath, chunksize):
 		"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize},
 		"rename":None, 
 		})
-	data["HANSEN_AFmask"] = ({
-		"fname":dpath+"/BurntArea/HANSEN/lossyear/Hansen_GFC-2018-v1.6_*_totalloss_SIBERIAatesacci_MODISAFmasked.nc",
-		'var':"lossyear", "gridres":"250m", "region":"Siberia", "timestep":"Annual", 
-		"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize},
-		"rename":None, 
-		})
+	# data["HANSEN_AFmask"] = ({
+	# 	"fname":dpath+"/BurntArea/HANSEN/lossyear/Hansen_GFC-2018-v1.6_*_totalloss_SIBERIAatesacci_MODISAFmasked.nc",
+	# 	'var':"lossyear", "gridres":"250m", "region":"Siberia", "timestep":"Annual", 
+	# 	"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize},
+	# 	"rename":None, 
+	# 	})
 	return data
 
 def datefixer(year, month, day):
@@ -475,8 +476,8 @@ def syspath():
 		# spath = "/media/ubuntu/Seagate Backup Plus Drive/Data51/VideoExports/"
 	elif 'ccrc.unsw.edu.au' in sysname:
 		dpath = "/srv/ccrc/data51/z3466821"
-		# chunksize = 20
-		chunksize = 5000
+		chunksize = 20
+		# chunksize = 5000
 	elif sysname == 'burrell-pre5820':
 		# The windows desktop at WHRC
 		dpath = "/mnt/f/Data51"
