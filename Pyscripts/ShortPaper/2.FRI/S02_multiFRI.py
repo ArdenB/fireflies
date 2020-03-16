9df7e0910090cfe748c613c1567d43fe190f1f47
Arden Burrell <arden.burrell@gmail.com>
2020-03-16 10:25:44 -0400

    tweaks

--- Pyscripts/ShortPaper/2.FRI/S02_multiFRI.py
+++ Pyscripts/ShortPaper/2.FRI/S02_multiFRI.py
@@ -196,10 +196,10 @@ def FRIcal(ds_ann, mask, dsn, force, ppath, mwbox, data, chunksize):
 		cleanup.append(tpath+tname)
 		cleanup.append(tpath+tMnme)
 
-	# print("Starting excess file cleanup at:", pd.Timestamp.now())
-	# for file in  cleanup:
-	# 	if os.path.isfile(file):
-	# 		os.remove(file)
+	print("Starting excess file cleanup at:", pd.Timestamp.now())
+	for file in  cleanup:
+		if os.path.isfile(file):
+			os.remove(file)
 
 def ANNcalculator(data, dsn, mask, force, ppath, dpath, chunksize):
 	""" Function to calculate the FRI 
@@ -374,12 +374,12 @@ def datasets(dpath, chunksize):
 		"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize},
 		"rename":None, 
 		})
-	# data["HANSEN_AFmask"] = ({
-	# 	"fname":dpath+"/BurntArea/HANSEN/lossyear/Hansen_GFC-2018-v1.6_*_totalloss_SIBERIAatesacci_MODISAFmasked.nc",
-	# 	'var':"lossyear", "gridres":"250m", "region":"Siberia", "timestep":"Annual", 
-	# 	"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize},
-	# 	"rename":None, 
-	# 	})
+	data["HANSEN_AFmask"] = ({
+		"fname":dpath+"/BurntArea/HANSEN/lossyear/Hansen_GFC-2018-v1.6_*_totalloss_SIBERIAatesacci_MODISAFmasked.nc",
+		'var':"lossyear", "gridres":"250m", "region":"Siberia", "timestep":"Annual", 
+		"start":2001, "end":2018, "rasterio":False, "chunks":{'time':1, 'longitude': chunksize, 'latitude': chunksize},
+		"rename":None, 
+		})
 	return data
 
 def datefixer(year, month, day):
